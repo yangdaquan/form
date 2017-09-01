98ae8eaa2c9d3452ce1add2210678b0a166c8af4
yangdaquan <18842576796@163.com>
2017-09-01 21:22:48 +0800

    改配置

--- config/deploy.sh
+++ config/deploy.sh
@@ -6,9 +6,9 @@ rm -f /etc/nginx/sites-enabled/default
 rm -f /etc/nginx/sites-available/default
 
 # 建立一个软连接
-ln -s -f /root/forum/conf/forum.conf /etc/supervisor/conf.d/forum.conf
+ln -s -f /root/forum/config/forum.conf /etc/supervisor/conf.d/forum.conf
 # 不要再 sites-available 里面放任何东西
-ln -s -f /root/forum/conf/forum.nginx /etc/nginx/sites-enabled/forum
+ln -s -f /root/forum/config/forum.nginx /etc/nginx/sites-enabled/forum
 
 # 重启服务器
 service supervisor restart
